9762c3fac79e4c0eeb69bb024615bbc1095f6702
Alexey Voznyuk <me@swizard.info>
2023-12-17 03:26:46 +0300

    impl `hyper::rt::{Read,Write}` for `Io`; example initial commit

---
+++ examples/primitive-curl/src/main.rs
@@ -0,0 +1,9 @@
+#[derive(Debug)]
+pub enum Error {
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,16 @@
 #![forbid(unsafe_code)]
 
+use std::{
+    io,
+    pin::{
+        Pin,
+    },
+    task::{
+        Poll,
+        Context,
+    },
+};
+
 use tokio::{
     net::{
         TcpStream,
@@ -37,4 +48,62 @@ struct IoTcp {
 
 struct IoTcpTls {
     stream: TokioIo<tokio_rustls::client::TlsStream<TcpStream>>,
+}
+
+impl hyper::rt::Read for Io {
+    fn poll_read(mut self: Pin<&mut Self>, cx: &mut Context<'_>, buf: hyper::rt::ReadBufCursor<'_>) -> Poll<Result<(), io::Error>> {
+        match &mut self.kind {
+            IoKind::Tcp(io) =>
+                Pin::new(&mut io.stream).poll_read(cx, buf),
+            IoKind::TcpTls(io) =>
+                Pin::new(&mut io.stream).poll_read(cx, buf),
+        }
+    }
+}
+
+impl hyper::rt::Write for Io {
+    fn poll_write(mut self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &[u8]) -> Poll<Result<usize, io::Error>> {
+        match &mut self.kind {
+            IoKind::Tcp(io) =>
+                Pin::new(&mut io.stream).poll_write(cx, buf),
+            IoKind::TcpTls(io) =>
+                Pin::new(&mut io.stream).poll_write(cx, buf),
+        }
+    }
+
+    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), io::Error>> {
+        match &mut self.kind {
+            IoKind::Tcp(io) =>
+                Pin::new(&mut io.stream).poll_flush(cx),
+            IoKind::TcpTls(io) =>
+                Pin::new(&mut io.stream).poll_flush(cx),
+        }
+    }
+
+    fn poll_shutdown(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), io::Error>> {
+        match &mut self.kind {
+            IoKind::Tcp(io) =>
+                Pin::new(&mut io.stream).poll_shutdown(cx),
+            IoKind::TcpTls(io) =>
+                Pin::new(&mut io.stream).poll_shutdown(cx),
+        }
+    }
+
+    fn is_write_vectored(&self) -> bool {
+        match &self.kind {
+            IoKind::Tcp(io) =>
+                io.stream.is_write_vectored(),
+            IoKind::TcpTls(io) =>
+                io.stream.is_write_vectored(),
+        }
+    }
+
+    fn poll_write_vectored(mut self: Pin<&mut Self>, cx: &mut Context<'_>, bufs: &[std::io::IoSlice<'_>]) -> Poll<Result<usize, io::Error>> {
+        match &mut self.kind {
+            IoKind::Tcp(io) =>
+                Pin::new(&mut io.stream).poll_write_vectored(cx, bufs),
+            IoKind::TcpTls(io) =>
+                Pin::new(&mut io.stream).poll_write_vectored(cx, bufs),
+        }
+    }
 }
